3552474d7b09e03c94e64650d67130030cb14575
francois.nouaille <francois.nouaille@MBPdefrnouaille.lan>
2019-11-15 09:35:21 +0100

    increase col size

--- vmGrid.py
+++ vmGrid.py
@@ -8,7 +8,7 @@ def add_vm_browser(form, on_selection):
     y, x = form.useable_space()
     return form.add(VmGrid, name = 'Instances',value = 0,
             additional_y_offset = 2, additional_x_offset = 2,# name = 'Instances',
-            max_height=int(y/2-2), column_width = 15, select_whole_line = True,
+            max_height=int(y/2-2), column_width = 17, select_whole_line = True,
             on_selection = on_selection, scroll_exit=True)
 
 class VmGrid(SelectableGrid):
