9280376f7b8f758a4ee3acd7e9f60169aa6db126
francois.nouaille <francois.nouaille@MBPdefrnouaille.lan>
2019-11-19 11:03:08 +0100

    Manage error if no .oapi_credentials.

--- profileSelector.py
+++ profileSelector.py
@@ -4,6 +4,7 @@ import npyscreen
 from pathlib import Path
 from osc_sdk_python import Gateway
 import main
+import os
 home = str(Path.home())
 
 
@@ -21,17 +22,20 @@ class CallbackFactory():
 
 class ProfileSelector(npyscreen.ActionFormV2):
     def create(self):
-        configFile = open(home + '/.oapi_credentials')
-        config = json.loads(configFile.read())
-        configFile.close()
-        self.add_widget(npyscreen.Textfield, value="Please select a cockpit profile:",
-                        editable=False)
-        btns = list()
-        for c in config:
-            bt = self.add_widget(npyscreen.ButtonPress, name=str(c))
-            btns.append(bt)
-        for bt in btns:
-            bt.whenPressed = CallbackFactory(self, bt.name)
+        if os.path.isfile(home + '/.oapi_credentials'):
+            configFile = open(home + '/.oapi_credentials')
+            config = json.loads(configFile.read())
+            configFile.close()
+            self.add_widget(npyscreen.Textfield, value="Please select a cockpit profile:",
+                            editable=False)
+            btns = list()
+            for c in config:
+                bt = self.add_widget(npyscreen.ButtonPress, name='->'+str(c))
+                btns.append(bt)
+                bt.whenPressed = CallbackFactory(self, str(c))
+        else:
+            self.add_widget(npyscreen.Textfield, value="Error, no file ~/.oapi_credentials found...",
+            editable=False)
 
     def create_control_buttons(self):
         self._add_button('cancel_button',
