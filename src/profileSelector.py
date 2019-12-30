6232804f6d7e06f2d16b1a86c3c4b73bc4f8f499
francois.nouaille <francois.nouaille@MacBook-Pro-de-francoisnouaille.local>
2019-12-30 14:39:46 +0100

    remove cockpit's logo

--- src/profileSelector.py
+++ src/profileSelector.py
@@ -12,67 +12,53 @@ from vmForm import VmForm
 
 home = str(Path.home())
 
-logo ="""
-                                        @@@ @@@                   &
-                                     @@@@@@ @@@@@@*               &
-                                 #@@@@@@@@@ @@@@@@@@@@            &
-                             @  @@@@@@@@@@@ @@@@@@@@@@@@@         &
-                             @@@@  @@@@@@@@ @@@@@@@@@             &
-                             @@@@@@@@ @@        @@                &         @@@@     @@@@       @@@@   @    @   @@@@@   @   @@@@@@
-                             @@@@@@@@@@                           &       @        @      @   @        @  @     @    @  @      @
-                             @@@@@@@@@@                           &       @        @      @   @        @@ @     @@@@@   @      @
-                             @@@@@@@@@@                           &         @@@@     @@@@       @@@@   @   @@   @       @      @
-                             @@@@@@  @@@@.    @@@@@*              &
-                             @@@ @@@@@@@@@@ @@@@@@@@@@@           &
-                              @@@@@@@@@@@@@ @@@@@@@@@@@@@@.       &
-                               @@@@@@@@@@@@ @@@@@@@@@@@@          &
-                                   @@@@@@@@ @@@@@@@@              &
-                                      @@@@@ @@@@@                 &
-                                          @ @@                    &  """
 
-
-class CallbackFactory():
+class CallbackFactory:
     def __init__(self, form, name):
         self.form = form
         self.name = name
 
     def __call__(self):
-        main.GATEWAY = Gateway(**{'profile': self.name})
-        self.form.parentApp.addForm(
-            "Cockpit", VmForm, name="osc-cli-curses")
-        self.form.parentApp.switchForm('Cockpit')
+        main.GATEWAY = Gateway(**{"profile": self.name})
+        self.form.parentApp.addForm("Cockpit", VmForm, name="osc-cli-curses")
+        self.form.parentApp.switchForm("Cockpit")
 
 
 class ProfileSelector(npyscreen.ActionFormV2):
     def create(self):
         self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE] = main.exit
-        if os.path.isfile(home + '/.oapi_credentials'):
-            configFile = open(home + '/.oapi_credentials')
+        if os.path.isfile(home + "/.oapi_credentials"):
+            configFile = open(home + "/.oapi_credentials")
             config = json.loads(configFile.read())
             configFile.close()
-            self.add_widget(npyscreen.Textfield, value="Please select a cockpit profile:",
-                            editable=False)
+            self.add_widget(
+                npyscreen.Textfield,
+                value="Please select a cockpit profile:",
+                editable=False,
+            )
             btns = list()
             for c in config:
-                bt = self.add_widget(npyscreen.ButtonPress, name='->'+str(c))
+                bt = self.add_widget(npyscreen.ButtonPress, name="->" + str(c))
                 btns.append(bt)
                 bt.whenPressed = CallbackFactory(self, str(c))
         else:
-            self.add_widget(npyscreen.Textfield, value="Error, no file ~/.oapi_credentials found...",
-                            editable=False)
-        self.add_widget(npyscreen.MultiLineEdit, value=logo,
-            editable=False, multiline = True)
+            self.add_widget(
+                npyscreen.Textfield,
+                value="Error, no file ~/.oapi_credentials found...",
+                editable=False,
+            )
 
     def create_control_buttons(self):
-        self._add_button('cancel_button',
-                         self.__class__.CANCELBUTTON_TYPE,
-                         'Exit',
-                         0 - self.__class__.CANCEL_BUTTON_BR_OFFSET[0],
-                         0 -
-                         self.__class__.CANCEL_BUTTON_BR_OFFSET[1] - len(
-                             self.__class__.CANCEL_BUTTON_TEXT),
-                         None
-                         )
+        self._add_button(
+            "cancel_button",
+            self.__class__.CANCELBUTTON_TYPE,
+            "Exit",
+            0 - self.__class__.CANCEL_BUTTON_BR_OFFSET[0],
+            0
+            - self.__class__.CANCEL_BUTTON_BR_OFFSET[1]
+            - len(self.__class__.CANCEL_BUTTON_TEXT),
+            None,
+        )
 
     def on_cancel(self):
         main.exit()
