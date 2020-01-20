ec0f7ce6f838849309d376c4ebce51a1b9739e18
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-01-20 16:31:34 +0100

    wip: inputBox for AKSK

--- src/inputBox.py
+++ src/inputBox.py
@@ -23,3 +23,22 @@ def readString(default_value="Input Text", title="Message", form_color='STANDOUT
         return tf.value
     else:
         return None
+
+def readAKSK(title="Please type your AKSK", form_color='STANDOUT'):
+
+    F = ConfirmCancelPopup(name=title, color=form_color)
+    F.preserve_selected_widget = True
+    tf = F.add(npyscreen.TitleText, name = "ACCESS KEY:")
+    sk = F.add(npyscreen.TitleText, name = "SECRET KEY:")
+    region = F.add_widget(
+        npyscreen.TitleCombo,
+        name="REGION:",
+        values="eu-west-2 eu-west-1".split(),
+        value=0,
+        )
+    tf.width = tf.width - 1
+    F.edit()
+    if F.value is True:
+        return [tf.value, sk.value, region.values[region.value]]
+    else:
+        return None
