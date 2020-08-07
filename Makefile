2fc6110123db48ed4b8b2fe72566ccafb4023b5d
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-08-07 16:08:29 +0200

    setup.sh: added a script to setup the project.

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 osc-tui:
-	python3 -m nuitka osc_tui/main.py -o exe --follow-imports -o osc-tui
+	python3 -m nuitka osc_tui/main.py -o exe --follow-imports --verbose -o osc-tui
+install:
+	sudo cp osc-tui /usr/local/bin
---
+++ setup.sh
@@ -0,0 +1,23 @@
+git clone https://github.com/outscale-dev/osc-tui.git
+cd osc-tui
+git submodule update --init
+python3 -m pip install virtualenv
+python3 -m virtualenv -p python3 env
+source env/bin/activate
+python3 -m pip install pyperclip
+python3 -m pip install autopep8
+python3 -m pip install nuitka
+python3 -m pip install https://github.com/outscale/osc-sdk-python/releases/download/0.9.15/osc_sdk_python-0.9.15-py3-none-any.whl
+cd npyscreen
+python3 setup.py build
+python3 setup.py install
+cd ..
+./configure.sh --dev
+read -p "Do you want to build and install osc-tui? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ] || [ $1 == "install" ]]
+then
+    make osc-tui
+    make install
+fi
+deactivate
