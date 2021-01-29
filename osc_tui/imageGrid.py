b27f377bb0c871550ff73142ceff8b63151b7c3d
Matthias Gatto <matthias.gatto@outscale.com>
2021-01-29 11:59:19 +0100

    add images
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ osc_tui/imageGrid.py
@@ -0,0 +1,27 @@
+import npyscreen
+import pyperclip
+
+import createVm
+import main
+import popup
+import selectableGrid
+import virtualMachine
+
+
+class ImageGrid(selectableGrid.SelectableGrid):
+    def __init__(self, screen, *args, **keywords):
+        super().__init__(screen, *args, **keywords)
+        self.col_titles = ["ID", "Description", "Size (Gb)", "Volume"]
+
+        def on_selection(line):
+            popup.editImage(self.form, line)
+
+        self.on_selection = on_selection
+
+    def refresh(self):
+        groups = main.GATEWAY.ReadImages(form=self.form)['Images']
+        values = list()
+        for g in groups:
+            values.append([g['ImageId'], g['Description'],
+                           g['ImageName'], g['ImageType']])
+        self.values = values
