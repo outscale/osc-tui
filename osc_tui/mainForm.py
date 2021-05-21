39ae3c46b7586dc423d1c221dc74e5ce623503e2
ogassotl <octave.gassot-laffont@outscale.com>
2021-05-21 09:13:14 +0000

    Replacing - by empty space for menu

--- osc_tui/mainForm.py
+++ osc_tui/mainForm.py
@@ -175,7 +175,7 @@ class MainForm(npyscreen.FormBaseNew):
         def build_line(size):
             out = ''
             for i in range(0, size):
-                out = out + '-'
+                out = out + '‎'
             return out
         menu_desc = (
             "INSTANCES SECURITY VOLUMES SNAPSHOT KEYPAIRS IMAGES LBUs VPCs(nets) NET-ACCESS-POINT NET-PEERING GPU REFRESH EXIT " +
