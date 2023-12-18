97affc60416b287e064045d71e0927b8704f10e0
matthias.gatto <matthias.gatto@outscale.com>
2023-12-18 14:35:44 +0100

    release 23.12.00
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

--- osc_tui/main.py
+++ osc_tui/main.py
@@ -33,7 +33,7 @@ except (Exception,KeyboardInterrupt):
 # Because it's cool but also a DDOS attack :)
 # So let's be cool with the API --> No auto refresh!
 POLL_ENABLED = False
-VERSION = 230900
+VERSION = 231200
 # GLOBALS METHODS
 
 CURRENT_GRID=None
