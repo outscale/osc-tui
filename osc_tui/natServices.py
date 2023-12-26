a83b75f89a56568ef85826072ebdb72fdd158231
matthias.gatto <matthias.gatto@outscale.com>
2023-12-26 16:26:05 +0100

    NatServices: show state, net_id, subnet_id
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

--- osc_tui/natServices.py
+++ osc_tui/natServices.py
@@ -5,15 +5,17 @@ from osc_tui import selectableGrid
 class natServicesGrid(selectableGrid.SelectableGrid):
     def __init__(self, screen, *args, **keywords):
         super().__init__(screen, *args, **keywords)
-        self.col_titles = ["Id"]
+        self.col_titles = ["Id", "State", "NetId", "SubnetId"]
 
     def refresh_call(self, name_filter=None):
         groups = main.GATEWAY.ReadNatServices(form=self.form)['NatServices']
         return groups
 
     def refresh(self):
-        groups = main.do_search(self.data.copy(), ["NatServiceId"])
+        groups = main.do_search(self.data.copy(), ["NatServiceId", "State", "NetId", "SubnetId"])
         values = list()
         for g in groups:
-            values.append([g['NatServiceId']])
+            values.append([g['NatServiceId'], g['State'],
+                           g["NetId"] if "NetId" in g else "unliked",
+                           g["SubnetId"] if "SubnetId" in g else "unliked"])
         self.values = values
