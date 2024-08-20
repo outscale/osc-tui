2797a106ac9fed53859518f4b5af7cd9a830ffb7
matthias.gatto <matthias.gatto@outscale.com>
2024-08-20 14:20:57 +0200

    fix description in setup.py
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 # -*- coding:utf-8 -*-
+import os
 from setuptools import find_packages, setup
+
+def get_long_description():
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(root_path, 'README.md'), 'r') as fd:
+        return fd.read()
+
 
 setup(
     name='osc-tui',
     version="24.06.0",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     author='Outscale SAS',
     author_email='opensource@outscale.com',
